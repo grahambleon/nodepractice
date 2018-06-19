9f2f74c4d6de6071529d4d6d35f8565d52c05fe5
Graham Gilpatric <grahamwithag@gmail.com>
2018-06-18 23:52:08 -0400

    work

--- asyncIO.js
+++ asyncIO.js
@@ -2,8 +2,10 @@ let fs = require('fs')
 
 let filePath = process.argv[2];
 
-function countLines(callBack) {
-  fs.readFile(filePath, function doneReading(err, fileContents){
+function doneReading(err, fileContents){
 
-  })
+}
+
+function countLines(callBack) {
+  fs.readFile(filePath, doneReading())
 }
